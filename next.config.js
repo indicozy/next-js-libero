dbb370b29565faeb2be47f92da777d29b8bc2260
indicozy <berkutkarlibai@gmail.com>
2022-04-06 01:36:33 +0600

    kkk

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["wp.liberoproject.kz", "liberoproject.kz", "secure.gravatar.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: '/feed',
+        destination: 'https://wp.liberoproject.kz/feed',
+        permanent: false,
+      },
+    ]
+  },
 };
